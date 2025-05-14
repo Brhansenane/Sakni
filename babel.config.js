2fd7d34ceb00d131b291e51ff028b17b91882bb9
Brhansenane <brhantk1@gmail.com>
2025-05-15 02:23:58 +0300

    update fainall update12

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    ['module:react-native-dotenv', {
+      moduleName: '@env',
+      path: '.env',
+      safe: true,
+      allowUndefined: false
+    }],
+    'expo-router/babel'
+  ]
 };
